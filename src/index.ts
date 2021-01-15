2298703a430963139a6ab275c184b0953f91b758
pr <pr>
2021-01-15 15:23:20 +0800

    chore: catch error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Schema } from '@hapi/joi';
 import { JSONSchema4 } from 'json-schema';
-import { compile } from 'json-schema-to-typescript'
+import { compile } from 'json-schema-to-typescript';
 import transformer from './transformer';
 
 export interface Options {
@@ -12,25 +12,26 @@ export interface Options {
 export const defaultOptions = {
   interfaceName: 'JoiTypes',
   bannerComment: '',
-}
+};
 
 /**
  * convert into types
  */
 export default async (schema: Schema, options: Options = defaultOptions) => {
-  const opts = { ...defaultOptions, ...options }
+  const opts = { ...defaultOptions, ...options };
   let jsonSchema: JSONSchema4 = {
     type: 'any',
-  }
+  };
   try {
     jsonSchema = transformer(schema, opts);
+    const { bannerComment, interfaceName } = opts;
+    const types = await compile(jsonSchema, interfaceName as string, { bannerComment });
+    return types;
   } catch (e) {
     console.warn('[joi2types] error', e);
   }
-  const { bannerComment, interfaceName } = opts;
-  const types = await compile(jsonSchema, interfaceName as string, { bannerComment });
-  return types;
-}
+  return '';
+};
 
 export const jsonSchema2Types = compile;
 export const joi2JsonSchema = transformer;
