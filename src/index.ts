23f34ba62607402b49128cff8dbd5c920fb7d34e
ycjcl868 <45808948@qq.com>
2021-01-15 16:06:28 +0800

    chore: remove catch

--- src/index.ts
+++ src/index.ts
@@ -21,22 +21,14 @@ export const defaultOptions = {
  */
 export default async (schema: Schema, options: Options = defaultOptions) => {
   const opts = { ...defaultOptions, ...options };
-  let jsonSchema: JSONSchema4 = {
-    type: 'any',
-  };
-  try {
-    jsonSchema = transformer(schema, opts);
-    const { bannerComment, interfaceName, format } = opts;
-    const types = await compile(jsonSchema, interfaceName as string, {
-      bannerComment,
-      unknownAny: false,
-      format,
-    });
-    return types;
-  } catch (e) {
-    console.warn('[joi2types] error', e);
-  }
-  return '';
+  const jsonSchema: JSONSchema4 = transformer(schema, opts);
+  const { bannerComment, interfaceName, format } = opts;
+  const types = await compile(jsonSchema, interfaceName as string, {
+    bannerComment,
+    unknownAny: false,
+    format,
+  });
+  return types;
 };
 
 export const jsonSchema2Types = compile;
